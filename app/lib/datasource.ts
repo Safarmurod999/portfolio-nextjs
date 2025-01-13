5f798322fde47efca098412aec8fc2b5731da59d
Safarmurod999 <supermen0904@gmail.com>
2025-01-13 16:17:17 +0500

    pem file changed

--- app/lib/datasource.ts
+++ app/lib/datasource.ts
@@ -56,14 +56,39 @@ export const connectToDatabase = async () => {
       password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME,
       synchronize: true,
-      entities: [Projects, Users, Categories, Leads,Education],
+      entities: [Projects, Users, Categories, Leads, Education],
       migrations: [],
       subscribers: [],
 
       extra: {
         ssl: {
           rejectUnauthorized: false,
-          ca: readFileSync(path.join(process.cwd(), "ca.pem")).toString(),
+          ca: `-----BEGIN CERTIFICATE-----
+MIIEQTCCAqmgAwIBAgIUOsV7MFbPZdIlK9k2SBTJXYGTxqIwDQYJKoZIhvcNAQEM
+BQAwOjE4MDYGA1UEAwwvMDI3YjQwNzMtYmViMy00YjU1LTliODktNWRlOTJmMDI4
+ZGQ3IFByb2plY3QgQ0EwHhcNMjQxMjA3MTY1NDA4WhcNMzQxMjA1MTY1NDA4WjA6
+MTgwNgYDVQQDDC8wMjdiNDA3My1iZWIzLTRiNTUtOWI4OS01ZGU5MmYwMjhkZDcg
+UHJvamVjdCBDQTCCAaIwDQYJKoZIhvcNAQEBBQADggGPADCCAYoCggGBAOo49gsx
+P8GPYT6kcgDkLKbehDxYXzoquIrXrsnpM4BATocbPPcVeENQ+P8DCzPIUxNCG5uM
+9rgy9QECyHlil8ZZTLcpcav/GmajRDDRzcp5DxrZ/GnU8baLu2daDMUmTxOmHevE
+qn4qhnrXpCss17xnNalOwtQS3eljaROuvA+wjvZrwoBhcw8sVjQLqwDxRvIp674R
+UYVeyHaqmydY1RFFL30N0biMAymkhttGCebndII6oAxRgBpSg84KcqwJEwttVngc
+ux0FuD6T7NlLtJUbOUVk5CHTwUl7en/e8rt3c+rji/z2t1URbEJRj69NRRyT/3iW
+snYCMdV3Gknlv2ci/yt/pUIn4+c7E9RmJGMFWaSMCEGBkyw7rOP/2L4a3ijMG/1t
+xuLEJfAZ04bi8KcIsKxmQLssWZP2+ufJ4V8YcNV8PMNkWciFKrKlHUGEK8eNw/ob
+ra2f0ObnTxloUcRHccZ7gwdo0nvISWWjDGWudPFIHVJvUnHVEPexpGhuRQIDAQAB
+oz8wPTAdBgNVHQ4EFgQUPma9SA2BmJBDspYL4GfYs7umjzgwDwYDVR0TBAgwBgEB
+/wIBADALBgNVHQ8EBAMCAQYwDQYJKoZIhvcNAQEMBQADggGBAKPpOt+Ttr2fCP1g
+2Vmz0dt/HTpodmRVq+amM58JWDyB93W0Cey/F9H833QPU3G1fZHyj//vFXmUPnOO
+AtUN22Y5z6p77EU8f+GdHx39XuCbwmAql+p6vYPgJ0wyr+m8LxUdmXjdTRQiM4gH
+ss7ByiFw7a1cfFwG3VaiCPLgFe/h9aD1YIhjV7ftzrAI7+O8ihyRcnu/90FD01zL
+MR8/IZkY2BHdsxBsqvnFwi0zQe9uPoL7LRikXDz8vAGeNLheHEgAmbp+B+M5pDqW
+c4ykX9VNKPa6wKeaPmv9IVQa2mF8CLdGYZdCCoXBhPHbXUNKcFkSc1q/4np5DcvC
+xPBQDmYI3iQIvwN24M7t5kcbIC6o/PytNzNljJMYSkSs8ovnlzxBp6AR8pNUHFKH
+6f7hf73S9Z6pBvTMhXqidepUzCFNtTMQrRUQOW3VWOV5IY/uNq10kzfT/Tdk5xap
+3LFYvqvvjKtIwxZU+d2r+FWi4xE0m3001bxRRIBCTXl6ujUlxw==
+-----END CERTIFICATE-----
+`,
         },
       },
     });
